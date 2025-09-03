c3a24bcacf868f0ac68fc15a49dcb7bb183942c8
Rahul Singh <rahul274301@gmail.com>
2025-09-03 13:46:20 +0530

    feat: add background beams and shooting stars components, update package.json and vite.config.js

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,25 @@
-import { useState } from 'react'
+import React from "react";
+import { BackgroundBeamsWithCollision } from "./components/ui/background-beams-with-collision";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+export default function App() {
   return (
-    <>
-    <div>Coming Soon</div>
-    </>
-  )
+    <BackgroundBeamsWithCollision>
+      <h2
+        className="text-2xl relative z-20 md:text-4xl lg:text-7xl font-bold text-center text-black dark:text-white font-sans tracking-tight">
+        Hello,I am Rahul!{" "}
+        <div
+          className="relative mx-auto inline-block w-max [filter:drop-shadow(0px_1px_3px_rgba(27,_37,_80,_0.14))]">
+          <div
+            className="absolute left-0 top-[1px] bg-clip-text bg-no-repeat text-transparent bg-gradient-to-r py-4 from-purple-500 via-violet-500 to-pink-500 [text-shadow:0_0_rgba(0,0,0,0.1)]">
+            <span className="">Software Engineer.</span>
+          </div>
+          <div
+            className="relative bg-clip-text text-transparent bg-no-repeat bg-gradient-to-r from-purple-500 via-violet-500 to-pink-500 py-4">
+            <span className="">Software Engineer.</span>
+          </div>
+        </div>
+        <div className="text-gray-400">Adding Content Soon</div>
+      </h2>
+    </BackgroundBeamsWithCollision>
+  );
 }
-
-export default App
